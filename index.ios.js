5cb0c081a306c913829b4680b13d02544511bf85
haz111 <haz111pl@gmail.com>
2016-04-09 12:00:01 +0200

    Unified android and ios version

--- index.ios.js
+++ index.ios.js
@@ -3,50 +3,10 @@
  * https://github.com/facebook/react-native
  */
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import React, {
+   AppRegistry,
+ } from 'react-native';
 
-class slavangard_app extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+ import SlavangardApp from './components/Main';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('slavangard_app', () => slavangard_app);
+AppRegistry.registerComponent('slavangard_app', () => SlavangardApp);
