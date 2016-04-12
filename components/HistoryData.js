483918a6b0af53e8e3d58f2b597d32db97808212
haz111 <haz111pl@gmail.com>
2016-04-12 18:01:35 +0200

    changes in HistoryData json format

--- components/HistoryData.js
+++ components/HistoryData.js
@@ -5,55 +5,46 @@ HistoryData.editions = [
     "no": 1,
     "year": 2007,
     "description": "opis 2007",
-    "movies": [],
     "people": "ludzie 2007"
   }, {
     "no": 2,
     "year": 2008,
     "description": "",
-    "movies": "",
     "people": ""
   }, {
     "no": 3,
     "year": 2009,
     "description": "",
-    "movies": "",
     "people": ""
   }, {
     "no": 4,
     "year": 2010,
     "description": "opis 2010",
-    "movies": "filmy 2010",
     "people": "ludzie 2010"
   }, {
     "no": 5,
     "year": 2011,
     "description": "",
-    "movies": "",
     "people": ""
   }, {
     "no": 6,
     "year": 2012,
     "description": "",
-    "movies": "",
     "people": ""
   }, {
     "no": 7,
     "year": 2013,
     "description": "",
-    "movies": "",
     "people": ""
   }, {
     "no": 8,
     "year": 2014,
     "description": "",
-    "movies": "",
     "people": ""
   }, {
     "no": 9,
     "year": 2015,
     "description": "",
-    "movies": "",
     "people": ""
   }
 ]
